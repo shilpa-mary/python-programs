3e5e224bbb0c38b43ed33713db8aeaeda3ddb52b
shilpa-mary <118098875+shilpa-mary@users.noreply.github.com>
2022-12-12 15:09:51 +0530

    Create fibanocci.py

---
+++ fibanocci.py
@@ -0,0 +1,13 @@
+def fibanocci(num):
+    n1=0
+    n2=1
+    count=0
+    print(n1)
+    print(n2)
+    while(count<num-2):
+        n3=n1+n2
+        print(n3)
+        n1=n2
+        n2=n3
+        count=count+1
+fibanocci(num)
